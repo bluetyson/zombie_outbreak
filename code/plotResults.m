2eb656d5c5be0c6cc204e5ad558eb8b9697d6260
Matthieu Mottet <mottetm@student.ethz.ch>
2012-11-14 17:32:45 +0100

    Working model updated
    Corrections to plotResults made

--- code/plotResults.m
+++ code/plotResults.m
@@ -7,12 +7,16 @@ function plotResults( ~ )
     for i = 1:3
        
         subplot( 4, 4, ( i * 4 - 3 ):( i * 4 - 2 ) );
-        plot( x, dump.S( i, : ), x, dump.Z( 1, : ), x, dump.R( i, : ) );
+        plot( x, dump.S( i, : ), x, dump.Z( i, : ), x, dump.R( i, : ) );
         ylim( [ 0 dump.S( 4 ) ] );
+        xlabel( 'Time' );
+        ylabel( 'Population' );
         title( [ 'State ', int2str( i ) ] );
     
         subplot( 4, 4, ( i * 4 - 1 ):( i * 4 ) );
         plot( x, dump.dS( i, : ), x, dump.dZ( i, : ), x, dump.dR( i, : ) );
+        xlabel( 'Time' );
+        ylabel( 'Population Variation' );
         title( [ 'State ', int2str( i ) ] );
 	
     end
@@ -20,11 +24,15 @@ function plotResults( ~ )
 	subplot( 4, 4, 13:14 );
 	plot( x, dump.S( 4, : ), x, dump.Z( 4, : ), x, dump.R( 4, : ) );
 	ylim( [ 0 dump.S( 4 ) ] );
+    xlabel( 'Time' );
+    ylabel( 'Population' );
 	legend( 'Susceptible', 'Zombie', 'Removed' );
 	title( 'World population' );
     
 	subplot( 4, 4, 15:16 );
 	plot( x, dump.dS( 4, : ), x, dump.dZ( 4, : ), x, dump.dR( 4, : ) );
+    xlabel( 'Time' );
+    ylabel( 'Population Variation' );
 	title( 'State 1' );
 
 
